192e8edc1e908eb2ee5f0443939ac1c74c8f3e52
edizzlla <edi.sulo01@gmail.com>
2025-08-23 16:36:43 +0200

    Added the header and styled it with TailwindCSS

---
+++ transactions-app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="fixed top-0 left-0 w-full bg-green-950 text-white h-16 flex items-center justify-between px-6 shadow-md z-10">
+      <h2 className="text-2xl font-bold">Transactions</h2>
+    </header>
+  );
+}
