35efee6aa7693e8d85c995edb6c268aaa7e41950
edizzlla <edi.sulo01@gmail.com>
2025-08-27 10:07:59 +0200

    changed some styling and fixed the mobile view bug

--- transactions-app/src/App.jsx
+++ transactions-app/src/App.jsx
@@ -5,16 +5,22 @@ import Home from "./pages/Home.jsx";
 
 function App() {
   const [sidebarOpen, setSideBarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSideBarOpen(!sidebarOpen);
+  };
+
   return (
     <>
       <Header />
       <Sidebar isOpen={sidebarOpen} onClose={() => setSideBarOpen(false)} />
       <Home />
       <button
-        onClick={() => setSideBarOpen(true)}
-        className="md:hidden fixed top-20 left-4 z-40 bg-blue-950 text-stone-50 p-2 rounded shadow"
+        onClick={toggleSidebar}
+        className="md:hidden fixed top-20 right-4  z-40 bg-blue-950 text-stone-50 p-2 rounded shadow"
+        aria-label={sidebarOpen ? "Close menu" : "Open menu"}
       >
-        ☰
+        {sidebarOpen ? "✕" : "☰"}
       </button>
     </>
   );
--- transactions-app/src/components/Sidebar.jsx
+++ transactions-app/src/components/Sidebar.jsx
@@ -1,26 +1,32 @@
 export default function Sidebar({ isOpen, onClose }) {
+  let classes = "block p-2 hover:bg-stone-300 rounded";
+
   return (
     <>
-     
       {isOpen && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-20 md:hidden"
+          className="fixed inset-0 bg-opacity-25 z-20 md:hidden"
           onClick={onClose}
         ></div>
       )}
 
       <aside
-        className={`fixed top-16 left-0 w-64 h-full bg-gray-800 text-white transform ${
-          isOpen ? 'translate-x-0' : '-translate-x-full'
+        className={`fixed top-16 left-0 w-64 h-full bg-gray-200 text-white transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
         } md:translate-x-0 transition-transform duration-300 ease-in-out z-30`}
       >
-        <nav className="p-4 text-white space-y-4">
-          <a href="#Home" className="block p-2 text-white hover:bg-gray-700 rounded">Home</a>
-          <a href="#CreateNewTransaction" className="block p-2 text-white hover:bg-gray-700 rounded">Create New Transaction</a>
-          <a href="#Login/Signup" className="block p-2 text-white hover:bg-gray-700 rounded">Login/Signup</a>
-          
+        <nav className="p-4 text-stone-600 font-bold space-y-4">
+          <a href="#Home" className={classes}>
+            Home
+          </a>
+          <a href="#CreateNewTransaction" className={classes}>
+            Create New Transaction
+          </a>
+          <a href="#Login/Signup" className={classes}>
+            Login/Signup
+          </a>
         </nav>
       </aside>
     </>
-  )
+  );
 }
